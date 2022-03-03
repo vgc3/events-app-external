fe8bd2abca7ef80bb2b621a86ae23ac805546678
vgc3 <vgc@deloitte.com>
2022-03-03 10:56:44 +0000

    1st change

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 'use strict';
 
+//My First CHange GITHUB TEST
 console.log(`process.env.SERVER = ${process.env.SERVER}`);
 // get the environment variable, but default to localhost:8082 if its not set
 const SERVER = process.env.SERVER ? process.env.SERVER : "http://localhost:8082";
